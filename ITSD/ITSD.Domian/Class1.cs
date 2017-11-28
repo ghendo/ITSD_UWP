4c76b7937e7b3a774765a99a0fe273af1ea7c725
Graeme Henderson <ghendo@me.com>
2017-11-28 17:07:05 +1100

    27/11/17

---
+++ ITSD/ITSD.Domian/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSD.Domian
+{
+    public class Class1
+    {
+        public class States
+        {
+            public int StateId { get; set; }
+            public string StateName { get; set; }
+            public string Abbreviation { get; set; }
+        }
+
+    }
+}
